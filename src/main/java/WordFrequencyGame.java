5bf0c978c48a22509e70eaccfce7923f36639ac3
Jacky Lee <leeja12@oocl.com>
2024-11-20 14:23:15 +0800

    refactor: renaming getResult and input parameter

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,13 +11,13 @@ public class WordFrequencyGame {
 //    magic string
 
 
-    public String getResult(String inputStr) {
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
+    public String getWordFrequency(String sentence) {
+        if (sentence.split("\\s+").length == 1) {
+            return sentence + " 1";
         } else {
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = sentence.split("\\s+");
 
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
