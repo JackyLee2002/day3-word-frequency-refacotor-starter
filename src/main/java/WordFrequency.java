1e60c70f23edf41923d5a0f7c616831183846b47
Jacky Lee <leeja12@oocl.com>
2024-11-20 14:27:47 +0800

    refactor: rename WordFrequency

---
+++ src/main/java/WordFrequency.java
@@ -0,0 +1,21 @@
+
+public class WordFrequency {
+    private String word; // word
+    private int count;
+
+    //    smell: naming
+    public WordFrequency(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getWordCount() {
+        return this.count;
+    }
+
+
+}
